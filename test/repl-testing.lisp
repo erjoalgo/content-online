8ee4e2fddd590822aa3dea2d117e7cf3cfdd13ae
Ernesto Alfonso <erjoalgo@gmail.com>
2018-06-10 20:29:52 -0700

    add sample sexp to start service

--- test/repl-testing.lisp
+++ test/repl-testing.lisp
@@ -1,2 +1,8 @@
 (declaim (optimize (debug 3) (speed 0)))
 (setf hunchentoot:*catch-errors-p* nil)
+
+(progn (when *service*
+         (stop *service*))
+       (start (make-config :port 4244
+                           :oauth-client-secret-json-path
+                           "client_secret_REMOVED.apps.googleusercontent.com.json")))
