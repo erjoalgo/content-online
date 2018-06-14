d25d0fd12007671896562c5668f9db0174e6655a
Ernesto Alfonso <erjoalgo@gmail.com>
2018-06-14 14:31:56 -0700

    add sexp to clear sessions

--- test/repl-testing.lisp
+++ test/repl-testing.lisp
@@ -1,6 +1,9 @@
 (declaim (optimize (debug 3) (speed 0)))
 (setf hunchentoot:*catch-errors-p* nil)
 
+
+'(setf (hunchentoot::session-db (service-acceptor *service*)) nil); delete all sessions
+
 (defun unintern-shadowing-symbols (package)
   (let ((syms (package-shadowing-symbols package)))
     (dolist (sym syms) (unintern sym package))
