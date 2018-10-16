7cd84eb2f50ef388cac8f735dbe571d4bc09f48a
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-16 01:12:16 -0700

    use defapi to define youtube data client api

--- youtube-client.lisp
+++ youtube-client.lisp
@@ -1,21 +1,21 @@
-(def-api-endpoint comment-threads :defaults '((:part . "snippet")
-                                              (:max-results . "100")))
+(in-package #:yt-comments/client)
 
-(def-api-endpoint subscriptions :defaults '((:part . "snippet")
-                                            (:max-results . "50")))
+(defapi "https://www.googleapis.com/youtube/v3/"
 
-(def-api-endpoint playlists :defaults '((:max-results . "50")))
+    :get-depaginate
+  ((comment-threads :default-params '((:part . "snippet")
+                                      (:max-results . "100")))
+   (subscriptions :default-params '((:part . "snippet")
+                                    (:max-results . "50")))
+   (playlists :default-params '((:max-results . "50")))
+   (playlist-items :default-params '((:max-results . "50")))
+   (channels :default-params '((:max-results . "50")))
+   (videos :default-params '((:max-results . "50")))
+   (yt-search :resource-path "search")
+   (activities :default-params '((:max-results . "50"))))
 
-(def-api-endpoint playlist-items :defaults '((:max-results . "50")))
-
-(def-api-endpoint channels :defaults '((:max-results . "50")))
-
-(def-api-endpoint videos :defaults '((:max-results . "50")))
-
-;; due to conflict with search function...
-(def-api-endpoint search :fun-sym yt-search)
-
-(def-api-endpoint activities :defaults '((:max-results . "50")))
+  :delete
+  ())
 
 (defun delete-comment (api-login comment-id)
   "DELETE https://www.googleapis.com/youtube/v3/comments"
