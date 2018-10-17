4ba5bf2b67479812bac587ee624bb07ea6fa4f93
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-16 22:47:11 -0700

    add failed attempt at handler test
    - need to mock dynamically

---
+++ test/server-test.lisp
@@ -0,0 +1,25 @@
+(fiasco:define-test-package
+    #:youtube-comments/test
+  (:use #:yt-comments/server))
+
+(in-package #:youtube-comments/test)
+
+(defvar results-count-json-string
+  "{
+ \"kind\": \"youtube#commentThreadListResponse\",
+ \"etag\": \"\\\"XI7nbFXulYBIpL0ayR_gDh3eu1k/ZG21yj3BiKrd-_sPro4Uu2c55GM\\\"\",
+ \"pageInfo\": {
+  \"totalResults\": 0,
+  \"resultsPerPage\": 50
+ },
+ \"items\": []
+}
+")
+
+(deftest test-results-count-handler ()
+  (let* ((json (cl-json:decode-json-from-string results-count-json-string))
+         (count (yt-comments/server::results-count-handler json))
+)
+    (is (equal count "0"))))
+
+(run-package-tests :interactive t)
