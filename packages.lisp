861b4dd4198df636c6217fed31f0b6ca400b9b1a
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-16 22:12:21 -0700

    simplify to one package

--- packages.lisp
+++ packages.lisp
@@ -1,90 +1,29 @@
-(defpackage #:yt-comments/server-util
-  (:use :cl :cl-markup)
-  (:export #:js-lazy-element))
-
-(defpackage #:yt-comments/util
-  (:use :cl)
-  (:export #:read-file
-           #:retry-times
-           #:assoq
-           #:make-from-json-alist
-           #:->
-           #:with-json-paths
-           #:lisp-to-json-key
-           #:json-key-to-lisp
-           #:params
-           #:json-path-split
-           #:json-get-nested
-           #:json-get-nested-macro))
-
-(defpackage #:yt-comments/oauth
-  (:use :cl)
-  (:import-from #:yt-comments/util
-                #:read-file
-                #:make-from-json-alist
-                #:->
-                #:assoq
-                #:json-get-nested-macro
-                #:PARAMS)
-  (:export #:make-oauth-client-from-file
-           #:OAUTH-TOKEN-AUTH-HEADER
-           #:AUTH-SERVER-REDIRECT-URL
-           #:EXCHANGE-CODE-FOR-TOKEN
-           #:RESP-TOKEN-ACCESS-TOKEN
-           #:RESP-TOKEN-REFRESH-TOKEN))
-
-(defpackage #:yt-comments/client
-  (:use :cl)
-  (:import-from #:yt-comments/util
-                #:make-from-json-alist
-                #:retry-times
-                #:with-json-paths
-                #:lisp-to-json-key)
-  (:import-from #:yt-comments/oauth
-                #:oauth-token-auth-header)
-  (:export #:make-api-login
-           #:default-base-url
-           #:subscriptions
-           #:comment-threads
-           #:subscriptions
-           #:playlists
-           #:playlist-items
-           #:channels
-           #:videos
-           #:search
-           #:activities
-           #:channel-url
-           #:video-url
-           #:playlist-url
-           #:delete-comment))
-
 (defpackage #:yt-comments/server
   (:use :cl :cl-markup)
-  (:import-from #:yt-comments/util
-                #:with-json-paths
-                #:->
-                #:json-get-nested-macro
-                #:assoq)
-  (:import-from #:yt-comments/server-util
-                #:js-lazy-element)
-  (:import-from #:yt-comments/client
-                #:make-api-login
-                #:subscriptions
-                #:comment-threads
-                #:channel-url
-                #:video-url
-                #:playlist-url
-                #:delete-comment
-                #:playlists
-                #:playlist-items)
-  (:import-from #:yt-comments/oauth
-                #:make-oauth-client-from-file
-                #:auth-server-redirect-url
-                #:exchange-code-for-token
-                #:resp-token-access-token
-                #:resp-token-refresh-token)
-  (:import-from #:hunchentoot
-                #:session-value
-                #:redirect)
+
+  (:import-from
+   #:erjoalgo-webutil
+   #:with-json-paths
+   #:->
+   #:-json-get-nested
+   #:assoq)
+
+  (:import-from
+   #:erjoalgo-webutil/google
+   #:defapi
+   #:api-req
+   #:make-api-login
+   #:make-oauth-client-from-file
+   #:auth-server-redirect-url
+   #:exchange-code-for-token
+
+   #:resp-token-access-token
+   #:resp-token-refresh-token)
+
+  (:import-from
+   #:hunchentoot
+   #:session-value
+   #:redirect)
+
   (:export #:start))
 
