61a1207749937eb3aa30c5d03ff6c5e3a913c8ee
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-22 22:37:50 -0700

    remove trailing slash from base url

--- youtube-client.lisp
+++ youtube-client.lisp
@@ -1,20 +1,20 @@
 (in-package #:youtube-comments)
 
 (defvar youtube-api-base-url
-  "https://www.googleapis.com/youtube/v3/")
+  "https://www.googleapis.com/youtube/v3")
 
 (defapi youtube-api-base-url
     :get-depaginate
-  ((comment-threads-get "commentThreads" :default-params '((:part . "snippet")
-                                                           (:max-results . "100")))
-   (subscriptions-get "subscriptions" :default-params '((:part . "snippet")
-                                                        (:max-results . "50")))
-   (playlists-get "playlists" :default-params '((:max-results . "50")))
-   (playlist-items-get "playlistItems" :default-params '((:max-results . "50")))
-   (channels-get "channels" :default-params '((:max-results . "50")))
-   (videos-get "videos" :default-params '((:max-results . "50")))
-   (search-get "search")
-   (activities-get "activities" :default-params '((:max-results . "50"))))
+  ((comment-threads-get "/commentThreads" :default-params '((:part . "snippet")
+                                                            (:max-results . "100")))
+   (subscriptions-get "/subscriptions" :default-params '((:part . "snippet")
+                                                         (:max-results . "50")))
+   (playlists-get "/playlists" :default-params '((:max-results . "50")))
+   (playlist-items-get "/playlistItems" :default-params '((:max-results . "50")))
+   (channels-get "/channels" :default-params '((:max-results . "50")))
+   (videos-get "/videos" :default-params '((:max-results . "50")))
+   (search-get "/search")
+   (activities-get "/activities" :default-params '((:max-results . "50"))))
 
   :delete
   ())
@@ -26,7 +26,7 @@
 
 (defun delete-comment (api-login comment-id)
   "DELETE https://www.googleapis.com/youtube/v3/comments"
-  (youtube-api-req api-login "comments"
+  (youtube-api-req api-login "/comments"
                    `(("id" . ,comment-id))
                    :method :delete))
 
