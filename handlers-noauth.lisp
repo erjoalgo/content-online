5e67c6f024ee41765accb728e04c76cb5e8d67c9
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-23 19:52:31 -0700

    store www dispatcher in a var

--- handlers-noauth.lisp
+++ handlers-noauth.lisp
@@ -1,8 +1,9 @@
 (in-package #:youtube-comments)
 
-(push (hunchentoot::create-folder-dispatcher-and-handler
-       "www/" #P"./www/")
-      hunchentoot::*dispatch-table*)
 (defroutes dispatchers-noauth
     (((:get) "^/health/?$")
      (json-resp '((:status . "OK")))))
+
+(defparameter www-dispatcher
+  (hunchentoot::create-folder-dispatcher-and-handler
+ "/www/" #P"./www/"))
