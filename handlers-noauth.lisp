5b7890f3b56dc4ef3e18882a43e2d2d3ee447815
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-23 19:48:09 -0700

    migrate to defroutes

--- handlers-noauth.lisp
+++ handlers-noauth.lisp
@@ -1,8 +1,8 @@
-(define-regexp-route health-check ("^/health/?$")
-    "health check"
-  (json-resp '((:status . "OK"))))
 (in-package #:youtube-comments)
 
 (push (hunchentoot::create-folder-dispatcher-and-handler
        "www/" #P"./www/")
       hunchentoot::*dispatch-table*)
+(defroutes dispatchers-noauth
+    (((:get) "^/health/?$")
+     (json-resp '((:status . "OK")))))
