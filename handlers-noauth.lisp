fd95c18c48217a5bc75715f06bbb88e6bbf5658c
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-23 19:47:20 -0700

    add in-package

--- handlers-noauth.lisp
+++ handlers-noauth.lisp
@@ -1,6 +1,7 @@
 (define-regexp-route health-check ("^/health/?$")
     "health check"
   (json-resp '((:status . "OK"))))
+(in-package #:youtube-comments)
 
 (push (hunchentoot::create-folder-dispatcher-and-handler
        "www/" #P"./www/")
