a88456ddbd6f48509f95f8b5080ae16052033d13
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-24 20:59:40 -0700

    fxp explicit XHR

--- www/js/feed-history-parse.js
+++ www/js/feed-history-parse.js
@@ -1,10 +1,13 @@
 var videoIdUpdateTimer = null;
 
 var videoIdUpdateDelayMs = 250;
+
+// text area for input
 var textAreaId = "inner-html";
-var videoIdsTextAreaId = "video-ids";
+// read-only text area for display
 var videoIdsTextAreaDisplayId = "video-ids-display";
-var parsedIsCountLabel = "parse-count";
+
+var parsedIdsCountLabel = "parse-count";
 
 function scheduleVideoIdUpdate (  ) {
     if (videoIdUpdateTimer) {
@@ -15,16 +18,19 @@ function scheduleVideoIdUpdate (  ) {
 }
 
 function videoIdUpdate (  ) {
-    var text = document.getElementById(textAreaId).value;
-    var videoIds = parseVideoIds(text);
+    var videoIds = parseVideoIds();
     var lines = videoIds.join("\n");
     document.getElementById(videoIdsTextAreaDisplayId).innerText = lines;
-    document.getElementById(videoIdsTextAreaId).innerText = lines;
+    // document.getElementById(videoIdsTextAreaId).innerText = lines;
     var lab = "parsed "+videoIds.length+" video ids";
-    document.getElementById(parsedIsCountLabel).innerText = lab;
+    document.getElementById(parsedIdsCountLabel).innerText = lab;
 }
 
 function parseVideoIds ( text ) {
+    if (!text) {
+        text = document.getElementById(textAreaId).value;
+    }
+
     // /watch?v=q4...
     var matches = text.match(/watch[?]v=[^"\\&]+/g) || [];
     var unique = {};
@@ -34,4 +40,39 @@ function parseVideoIds ( text ) {
         unique[id] = true;
     }
     return Object.keys(unique);
+}
+
+// var aggregationInputName
+function postVideoIds (  ) {
+    var aggregation = document.querySelector('input[name=aggregation]:checked');
+    var videoIds = parseVideoIds();
+    if (!aggregation) {
+        alert("missing aggregation selection");
+    } else if (videoIds.length === 0) {
+        alert("empty video id list");
+    } else  {
+        var data = {
+            aggregation: aggregation.value,
+            videoIds: videoIds
+        };
+        console.log( "value of data: "+data );
+        var xhr = new XMLHttpRequest();
+        var postUrl = "/feed-history/video-ids";
+        xhr.open('POST', postUrl, /* */true);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4){
+                if ( xhr.status != 200) {
+                    var err = "POST "+postUrl+" failed: "+xhr.status + " "+xhr.responseText;
+                    console.log(err);
+                    alert(err);
+                    debugger;
+                }else{
+                    var resp = JSON.parse(xhr.responseText);
+                    window.location = resp.location;
+                }
+            }
+        };
+        xhr.send(JSON.stringify(data));
+    }
 }
