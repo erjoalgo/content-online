ca86c170a323844ecd46806eec9c2655315e11e3
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-31 23:49:55 -0700

    fxp add logging

--- lisp/handler-util.lisp
+++ lisp/handler-util.lisp
@@ -27,7 +27,7 @@
   (with-gensyms (body-sym http-code-sym)
     `(multiple-value-bind (,body-sym ,http-code-sym)
          ,api-req-values
-       (vom:debug ",body-sym: ~A~%" ,body-sym)
+       (vom:debug "body-sym: ~A~%" ,body-sym)
        (if (not (eq ,ok-code ,http-code-sym))
            (progn
              (vom:warn "unexpected code: ~A ~A ~A~%"
